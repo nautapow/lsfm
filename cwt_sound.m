8393652dd94d96196c10d96bacfe7726cfab7078
nautapow <u234830@bcm.edu>
2021-11-02 15:38:10 -0500

    Update cwt_sound.m
    cwt multiple files

--- cwt_sound.m
+++ cwt_sound.m
@@ -1,16 +1,32 @@
-stim = stim(~cellfun('isempty', stim));
+wdir = pwd;
+cd ..
+mdir = pwd;
+cd (wdir)
 
-[~, f] = cwt(stim{1,1},200000, 'WaveletParameters',[32,1280]);
-
-for x=1:length(stim)
-    wt{x} = cwt(stim{1,x},200000, 'WaveletParameters',[32,1280]);
-    wt{x} = abs(wt{x});
+list = dir;
+names = {list.name};
+names = names(~ismember(names,{'.','..'}));
+for i = 1:length(names)
+    load(names{i});
     
-    for i = 1:length(f)
-        wt_t(i,:) = decimate(wt{x}(i,:), 800);
+    stim = stim(~cellfun('isempty', stim));
+
+    [~, f] = cwt(stim{1,1},200000, 'WaveletParameters',[32,1280]);
+
+    for x=1:length(stim)
+        wt{x} = cwt(stim{1,x},200000, 'WaveletParameters',[32,1280]);
+        wt{x} = abs(wt{x});
+
+        for i = 1:length(f)
+            wt_t(i,:) = decimate(wt{x}(i,:), 800);
+        end
+
+        wt{x}=wt_t
     end
-    
-    wt{x}=wt_t
-end
 
-save('/Users/POW/Desktop/python_learning/sound_cwt_date.mat', 'f', 'wt')
+    str = append(names{i}(1:12), '_cwt.mat');
+    sdir = append(mdir,'/cwt_sound')
+    save([(sdir),str], 'f', 'wt')
+    clear
+    
+end
