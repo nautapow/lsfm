78805e0dd8ab78b076666ea5bdf2af19ead48435
nautapow <u234830@bcm.edu>
2022-06-22 15:07:20 -0500

    Update puretone_analysis.py

--- puretone_analysis.py
+++ puretone_analysis.py
@@ -19,30 +19,28 @@ if  __name__ == "__main__":
     df = pd.read_csv('patch_list_E.csv', dtype={'date':str, '#':str})
     idx_puretone = df.index[df['type']=='Pure Tones']
     
-    #df_loc = 26
-    #if df_loc == 26:
-    for df_loc in idx_puretone:
-        try:
-            fdir = df['path'][df_loc]
-            filename = df['date'][df_loc]+'_'+df['#'][df_loc]
-            version = df['Version'][df_loc]
-            if version == 1:
-                t = Tdms_V1()
-                t.loadtdms(fdir, protocol=1, load_sound=False, precise_timing=True)
-            if version == 2:
-                t = Tdms_V2()
-                t.loadtdms(fdir, protocol=1, load_sound=False)
-                
-    
-            para = t.Para
-            resp = np.array(t.Rdpk)
-            #sound = t.rawS
-            #stim = t.Sound
+    df_loc = 43
+    if df_loc == 43:
+    #for df_loc in idx_puretone:
+        
+        fdir = df['path'][df_loc]
+        filename = df['date'][df_loc]+'_'+df['#'][df_loc]
+        version = df['Version'][df_loc]
+        if version == 1:
+            t = Tdms_V1()
+            t.loadtdms(fdir, protocol=1, load_sound=False, precise_timing=True)
+        if version == 2:
+            t = Tdms_V2()
+            t.loadtdms(fdir, protocol=1, load_sound=False)
             
-            puretone.tunning(resp, para, filename=filename, saveplot=True)
-            print(filename)
-        except:
-            pass
+
+        para = t.Para
+        resp = np.array(t.Rdpk)
+        #sound = t.rawS
+        #stim = t.Sound
+        
+        puretone.tunning(resp, para, filename=filename, saveplot=False)
+
        
         
         
