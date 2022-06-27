70fe54bba9819188b1a609e828b93f49060bcd2d
nautapow <u234830@bcm.edu>
2022-06-27 15:00:37 -0500

    Update lsfm_analysis.py

--- lsfm_analysis.py
+++ lsfm_analysis.py
@@ -21,18 +21,16 @@ import math
 if  __name__ == "__main__":
     df = pd.read_csv('patch_list_E.csv', dtype={'date':str, '#':str})
     idx_lsfm = df.index[df['type']=='Log sFM']
-    #tlsfm = [23,24,25,27,28,30,32,35,37,40,45,49]
-    tlsfm = [23,24,25,28,30,35,37,45,60,62,71,74]
-    bfs = [5000, 14000, 11200, 8800, 8000, 3500, 4500, 7000, 4500, 5700, 10500, 14000]
+    tlsfm = [23,24,25,28,30,35,37,45,49,60,62,63,71,73,74,76,78,81,82]
+    bfs = [6034,24000,15213,6976,6865,5827,4914,10940,13985,5864,5864,5864,15594,15594,15594,16965,8987,17515,17515]
+
     #tlsfm = [65,67,70,71,73,74,76]
     
-    slope_all = pd.DataFrame()
-    slope_bf = pd.DataFrame()
-    
-    #df_loc = 71
-    #if df_loc == 71:
-    #for i in range(len(tlsfm)):
-        i = 10
+    #df_loc = 76
+    #if df_loc == 76:
+    for i in range(len(tlsfm)):
+    #for df_loc in tlsfm:
+        #i = 2
         df_loc = tlsfm[i]
         filename = df['filename'][df_loc]
         cell_data = np.load(f'{filename}_data.npy', allow_pickle=True)
@@ -42,18 +40,24 @@ if  __name__ == "__main__":
         resp = cell_data.item().get('resp')
         slope_lags = cell_data.item().get('slope_lag')
         
-# =============================================================================
-#         txt = filename+'_slope'
-#         lsfm_slope.plot_slope_index(*lsfm_slope.slope_index(slope_lags, bfs[i]), txt, saveplot=False)
-#         txt = filename+'_direction'
-#         lsfm_slope.plot_slope_index(*lsfm_slope.direction_index(lsfm_slope.direction_map(slope_lags), bfs[i]), txt, saveplot=False)
-# =============================================================================
+        lags = np.linspace(0, 50, 11)
+        slope_lags = lsfm_slope.freq_slope_contour(stim, resp, para, lags=lags, filename=filename, plot=True, saveplot=True)
+        
+        if df['Version'][df_loc] == 1:
+            _ = lsfm_slope.freq_slope_contour(stim, resp, para, lags=lags, window=(1250,37500), filename=filename, plot=True, saveplot=True)
+        elif df['Version'][df_loc] == 2:
+            _ = lsfm_slope.freq_slope_contour(stim, resp, para, lags=lags, window=(1250,25000), filename=filename, plot=True, saveplot=True)
+        
+        txt = filename+'_slope'
+        lsfm_slope.plot_slope_index(*lsfm_slope.slope_index(slope_lags, bfs[i]), txt, saveplot=True)
+        txt = filename+'_direction'
+        lsfm_slope.plot_slope_index(*lsfm_slope.direction_index(lsfm_slope.direction_map(slope_lags), bfs[i]), txt, saveplot=True)
            
         
 # =============================================================================
-#         #savedata = []
+#         savedata = []
 #         fdir = df['path'][df_loc]
-#         filename = df['date'][df_loc]+'_'+df['#'][df_loc]
+#         filename = df['filename'][df_loc]
 #         version = df['Version'][df_loc]
 #         if version == 1:
 #             t = Tdms_V1()
@@ -72,13 +76,14 @@ if  __name__ == "__main__":
 #         elif version == 2:
 #             p = lsfm_psth.Psth_New(resp, para, filename)
 #             
-#         lags = np.linspace(0, 100, 51)
-#         slope_lags = lsfm.freq_slope_contour(stim, resp, para, lags=lags, filename=filename, plot=True, saveplot=True)        
+#         lags = np.linspace(0, 50, 11)
+#         slope_lags = lsfm_slope.freq_slope_contour(stim, resp, para, lags=lags, window=(2000,10000), filename=filename, plot=True, saveplot=False)        
 #         
 #         
 #         cell_data = {'stim':stim, 'resp':resp, 'para':para, 'slope_lags':slope_lags}
-#         np.save(f'{filename}_data.npy', cell_data)
+#         #np.save(f'{filename}_data.npy', cell_data)
 # =============================================================================
+
                 
                 
 
@@ -159,34 +164,6 @@ if  __name__ == "__main__":
 #         atf.resp_at_freq(nth_freq=True, plot=False)
 # =============================================================================
     
-        
- 
-        
-# =============================================================================
-#         """ average response in positive vs negative slope"""
-#         for tf in range(len(atf.target_freq)):   
-#             post, neg = [],[]
-#             for i,a in enumerate(atf.slopes[tf]):
-#                 if a > 0:
-#                     post.append(atf.windows[tf][i])
-#                 else:
-#                     neg.append(atf.windows[tf][i])
-#             
-#             mean_p = np.mean(post, axis=0)
-#             mean_p = mean_p - mean_p[1250]
-#             mean_n = np.mean(neg, axis=0)
-#             mean_n = mean_n - mean_n[1250]
-#             
-#             plt.plot(mean_p)
-#             plt.plot(mean_n)
-#             plt.axvline(x=1250, color='k', linestyle='--', alpha=0.5)
-#             ax = plt.subplot()
-#             txt = (f'{atf.filename} - {atf.target_freq[tf]} Hz.')
-#             ax.text(0,1.02, txt, horizontalalignment='left', transform=ax.transAxes)    
-#             #plt.savefig(f'slope_PvN-{atf.filename}-{atf.target_freq[tf]}.png', dpi=500)
-#             plt.show()
-#             plt.clf()
-# =============================================================================
 
     
     
@@ -229,18 +206,6 @@ if  __name__ == "__main__":
 #         inst_freqs.append(np.diff(insfreq))
 # =============================================================================
         
-    
-   
-    
-# =============================================================================
-#     wt_swap = np.swapaxes(wt_a, 0,1)
-#     fband = []
-#     for w in wt_swap:
-#         fband.append(w.max())
-#         
-#     plt.plot(fband)
-#     #plt.savefig('fir_flip_real.png', dpi=500)
-# =============================================================================
 
 
 # =============================================================================
@@ -254,67 +219,6 @@ if  __name__ == "__main__":
 #     cwt = scipy.io.loadmat('0730_fir_cwt.mat')
 # =============================================================================
 
-    
-    
-# =============================================================================
-# cwt = scipy.io.loadmat('/Users/POW/Desktop/python_learning/cwt_sound.mat')
-# f = cwt['f']
-# 
-# n_epochs = len(resp)
-# wt = []
-# R = []
-# for x in range(n_epochs):
-#     R.append(resp[x])
-#     wt.append(cwt['wt'][0][:][x][:])
-#     
-# #cwt_f = np.array(y.T)[:][0]
-# 
-# R = np.array(R)
-# wt = np.array(wt)
-# R = signal.resample(R, 500, axis=1)
-# P = wt**2
-# 
-# tmin = -0.1
-# tmax = 0.4
-# sfreq = 250
-# freqs = f.T[:][0]
-# 
-# train, test = np.arange(n_epochs - 1), n_epochs - 1
-# X_train, X_test, y_train, y_test = P[train], P[test], R[train], R[test]
-# X_train, X_test, y_train, y_test = [np.rollaxis(ii, -1, 0) for ii in
-#                                     (X_train, X_test, y_train, y_test)]
-# # Model the simulated data as a function of the spectrogram input
-# alphas = np.logspace(-3, 3, 7)
-# scores = np.zeros_like(alphas)
-# models = []
-# for ii, alpha in enumerate(alphas):
-#     rf = ReceptiveField(tmin, tmax, sfreq, freqs, estimator=alpha)
-#     rf.fit(X_train, y_train)
-# 
-#     # Now make predictions about the model output, given input stimuli.
-#     scores[ii] = rf.score(X_test, y_test)
-#     models.append(rf)
-# 
-# times = rf.delays_ / float(rf.sfreq)
-# 
-# # Choose the model that performed best on the held out data
-# ix_best_alpha = np.argmax(scores)
-# best_mod = models[ix_best_alpha]
-# coefs = best_mod.coef_[0]
-# best_pred = best_mod.predict(X_test)[:, 0]
-# 
-# # Plot the original STRF, and the one that we recovered with modeling.
-# 
-# plt.pcolormesh(times, rf.feature_names, coefs, shading='auto')
-# #plt.set_title('Best Reconstructed STRF')
-# #plt.autoscale(tight=True)
-# strf_o = {'time' : times, 'feature' : rf.feature_names, 
-#           'coef' : coefs}
-# plt.yscale('log')
-# plt.ylim(2000,90000)
-# plt.savefig('strf.png', dpi=300)
-# scipy.io.savemat('strf_out.mat', strf_o)
-# =============================================================================
 
 
 
