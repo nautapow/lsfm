d561bd4cfeb71e00ed0291c18a6f7f6677b627da
nautapow <u234830@bcm.edu>
2022-07-04 13:14:19 -0500

    Update lsfm_analysis.py

--- lsfm_analysis.py
+++ lsfm_analysis.py
@@ -22,46 +22,104 @@ if  __name__ == "__main__":
     df = pd.read_csv('patch_list_E.csv', dtype={'date':str, '#':str})
     idx_lsfm = df.index[df['type']=='Log sFM']
     tlsfm = [23,24,25,28,30,35,37,45,49,60,62,71,73,74,76,78,81,82]
+    #tlsfm = [37, 60]
     cell_note = pd.read_csv('cell_note_all.csv')
-    #lsfm.resp_overcell(df, tlsfm, saveplot=True)
+    lsfm.resp_overcell(df, tlsfm, saveplot=True)
     resp_at_freq_cell=[]
     
-    lag_all = cell_note['best_lag_all']
-    lag_first = cell_note['best_lag_bf']
-    
-    
+    #lsfm.best_lags()
 # =============================================================================
-#     df_loc = 30
-#     if df_loc == 30:
-#     #for df_loc in tlsfm:
-#         i = int([i for i,a in enumerate(tlsfm) if a == df_loc][0])
-#         filename = df['filename'][df_loc]
-#         version = df['Version'][df_loc]
-#         cell_data = np.load(f'{filename}_data.npy', allow_pickle=True)
-#         
-#         para = cell_data.item().get('para')
-#         stim = cell_data.item().get('stim')
-#         resp = cell_data.item().get('resp')
-#         slope_lags = cell_data.item().get('slope_lags')
-#         
-#         cf,band,modrate,_=zip(*para)
-#         band = sorted(set(band))
-#         cf = sorted(set(cf))
-#         modrate = sorted(set(modrate))
-#         
-#         n = cell_note.index[cell_note['filename']==filename][0]
-#         bf = cell_note['best frequency'].loc[n]
+#     bf = cell_note['best frequency']
+#     lag_all = cell_note['best_lag_all']
+#     lag_first = cell_note['best_lag_bf']
+#     bf_scale = [i/1000 for i in bf]
+#     
+#     fig, ax1 = plt.subplots(figsize=(4,6))
+#     im = ax1.scatter(np.zeros(len(lag_all)), lag_all, zorder=10, c=bf_scale, cmap='plasma', s=100)
+#     im = ax1.scatter(np.ones(len(lag_first)), lag_first, zorder=10, c=bf_scale, cmap='plasma', s=100)
+#     cbar = plt.colorbar(im)
+#     cbar.ax.set_ylabel('best frequency (kHz)', fontsize=16)
+#     cbar.ax.tick_params(axis='y', which='major', labelsize=14)
+#     for i in range(len(lag_all)):
+#         ax1.plot([0,1], [lag_all[i], lag_first[i]], c='k', zorder=0)
+#     plt.locator_params(axis='x', nbins=2)    
+#     ax1.set_xlim(-0.5,1.5)
+#     ax1.set_ylabel('lags (ms)', fontsize=16)
+#     ax1.set_xticks([0,1])
+#     ax1.set_xticklabels(['All Cross', 'First Cross'])
+#     ax1.tick_params(axis='both', which='major', labelsize=14)
 # =============================================================================
+    
+    df_loc = 28
+    if df_loc == 28:
+    #for df_loc in tlsfm:
+        i = int([i for i,a in enumerate(tlsfm) if a == df_loc][0])
+        filename = df['filename'][df_loc]
+        version = df['Version'][df_loc]
+        cell_data = np.load(f'{filename}_data.npy', allow_pickle=True)
+        
+        para = cell_data.item().get('para')
+        stim = cell_data.item().get('stim')
+        resp = cell_data.item().get('resp')
+        slope_lags = cell_data.item().get('slope_lags')
+        
+        cf,band,modrate,_=zip(*para)
+        band = sorted(set(band))
+        cf = sorted(set(cf))
+        modrate = sorted(set(modrate))
+        
+        n = cell_note.index[cell_note['filename']==filename][0]
+        bf = cell_note['best frequency'].loc[n]
         
         #resp_at_freq_cell = np.load('restrain_resp_at_freq_cell.npy', allow_pickle=True)
         #test = lsfm.nXing_cell(resp_at_freq_cell)
-                
-        #lsfm_psth.psth_wwo_bf(resp, para, bf, version, filename, saveplot=True)
+        
+
+
+# =============================================================================
+#         """PSTH"""
+#         if version == 1:
+#             p = lsfm_psth.Psth(resp, para, filename)
+#         elif version == 2:
+#             p = lsfm_psth.Psth_New(resp, para, filename)
+#             
+#         _,_,_ = p.psth_all(plot=False, saveplot=False)
+#         lsfm_psth.psth_wwo_bf(resp, para, bf, version, filename, saveplot=True)
+# =============================================================================
+        
+        
         
 # =============================================================================
+#         fdir = df['path'][df_loc]
+#         filename = df['filename'][df_loc]
+#         version = df['Version'][df_loc]
+#         if version == 1:
+#             t = Tdms_V1()
+#             t.loadtdms(fdir, protocol=0, load_sound=True, precise_timing=True)
+#         if version == 2:
+#             t = Tdms_V2()
+#             t.loadtdms(fdir, protocol=0, load_sound=True)
+#         
+#         #para = t.Para
+#         #resp = np.array(t.Rdpk)
+#         sound = t.rawS
+#         #stim = t.Sound
+#         target_FIR = f'E:\in-vivo_patch\FIR_list\FIR_{df["FIR"][df_loc]}.txt'
+#         
+#         with open(target_FIR, 'r') as file:
+#                  fir = np.array(file.read().split('\n')[:-1], dtype='float64')
+#         sound_re = lsfm.inv_fir(sound, fir)
+#         sound_re = lsfm.inv_fir(sound, fir)
+#         sound_re = t.cut(sound_re)
+#         scipy.io.savemat(f'{filename}_invfir4cwt.mat', {'stim':sound_re})
+# =============================================================================
+        
+
+# =============================================================================
+#         """plot SD-slope and SD-direction"""
 #         m, m_bf = lsfm_slope.slope_index(slope_lags, bf)
 #         d, d_bf = lsfm_slope.direction_index(lsfm_slope.direction_map(slope_lags), bf)
-#         lsfm_slope.plot_both_index(m, m_bf, d, d_bf, filename, plot=False, saveplot=True)
+#         lsfm_slope.plot_both_index(m, m_bf, d, d_bf, filename, plot=True, saveplot=True)
 #         #lsfm_slope.plot_both_index(*lsfm_slope.slope_index(slope_lags, bf), *lsfm_slope.direction_index(lsfm_slope.direction_map(slope_lags), bf), filename)
 # =============================================================================
         
@@ -72,28 +130,34 @@ if  __name__ == "__main__":
         
 # =============================================================================
 #         for i,p in enumerate(para):
-#             lsfm.stim_resp(i, stim[i], resp[i], p[:3], filename, saveplot=False)
+#             if i in [63,64,328]:
+#                 lsfm.stim_resp(i, stim[i], resp[i], p[:3], filename, saveplot=True)
 # =============================================================================
         
 
             
 # =============================================================================
-#         lags = np.linspace(0, 100, 51)
+#         lags = np.linspace(0, 100, 51)       
+#
+#         
 #         resp_at_freq = lsfm.resp_freq(stim, resp, para, lags, bf)
-#         _, best_lag = lsfm.at_freq_lag(resp_at_freq, filename=filename, plot=True, saveplot=True)
+#         _,_, best_lag = lsfm.at_freq_lag(resp_at_freq, filename=filename, plot=True, saveplot=True)
 # =============================================================================
-        
-                
+
+
 
         
-        
 # =============================================================================
 #         lags = np.linspace(0, 100, 51)
-#         slope_lags = lsfm_slope.freq_slope_contour(stim, resp, para, lags=lags, filename=filename, plot=False, saveplot=False)
+#         slope_lags = lsfm_slope.freq_slope_contour(stim, resp, para, lags=lags, filename=filename, plot=False, saveplot=True)
+# =============================================================================
+        
+        
+# =============================================================================
 #         cell_data = {'stim':stim, 'resp':resp, 'para':para, 'resp_by_para':resp_by_para, 'slope_lags':slope_lags}
 #         np.save(f'{filename}_data.npy', cell_data)
-#         
 # =============================================================================
+        
 
         
 # =============================================================================
@@ -120,64 +184,14 @@ if  __name__ == "__main__":
 #         lsfm_slope.plot_slope_index(*lsfm_slope.direction_index(lsfm_slope.direction_map(slope_range), bfs[i]), txt, saveplot=True)
 # =============================================================================
         
+
         
+        
+
+            
 # =============================================================================
-#         savedata = []
-#         fdir = df['path'][df_loc]
-#         filename = df['filename'][df_loc]
-#         version = df['Version'][df_loc]
-#         if version == 1:
-#             t = Tdms_V1()
-#             t.loadtdms(fdir, protocol=0, load_sound=True, precise_timing=True)
-#         if version == 2:
-#             t = Tdms_V2()
-#             t.loadtdms(fdir, protocol=0, load_sound=True)
-#         
-#         para = t.Para
-#         resp = np.array(t.Rdpk)
-#         sound = t.rawS
-#         stim = t.Sound
-#         
-#         if version == 1:
-#             p = lsfm_psth.Psth(resp, para, filename)
-#         elif version == 2:
-#             p = lsfm_psth.Psth_New(resp, para, filename)
-#             
 #         lags = np.linspace(0, 50, 11)
 #         slope_lags = lsfm_slope.freq_slope_contour(stim, resp, para, lags=lags, window=(2000,10000), filename=filename, plot=True, saveplot=False)        
-# =============================================================================
-        
-        
-                
-        
-# =============================================================================
-#         #p.psth_window((27500,30000), 'offset', tuning=None, saveplot=True, savenotes=True)
-#         test = p.psth_trend(saveplot=False, window=(3000,6000))
-#         
-#         df2 = pd.DataFrame(columns = ['bd', 'cf', 'y', 'err'])
-#         for i in range(len(test)):
-#             _df = pd.DataFrame(test[i], columns = ['bd', 'cf', 'y', 'err'])
-#             df2 = pd.concat([df2, _df])      
-#         
-#         df2.to_csv(f'{filename}_onset.csv', index=False)
-#         
-#         test = p.psth_trend(saveplot=False, window=(17500,22500))
-#         
-#         df3 = pd.DataFrame(columns = ['bd', 'cf', 'y', 'err'])
-#         for i in range(len(test)):
-#             _df = pd.DataFrame(test[i], columns = ['bd', 'cf', 'y', 'err'])
-#             df3 = pd.concat([df3, _df])      
-#         
-#         df3.to_csv(f'{filename}_sustain.csv', index=False)
-#         
-#         test = p.psth_trend(saveplot=False, window=(27500,30000))
-#         
-#         df4 = pd.DataFrame(columns = ['bd', 'cf', 'y', 'err'])
-#         for i in range(len(test)):
-#             _df = pd.DataFrame(test[i], columns = ['bd', 'cf', 'y', 'err'])
-#             df4 = pd.concat([df4, _df])      
-#         
-#         df4.to_csv(f'{filename}_offset.csv', index=False)
 # =============================================================================
         
 
@@ -263,19 +277,43 @@ if  __name__ == "__main__":
 # =============================================================================
         
 
-
 # =============================================================================
+# 
 #     with open('FIR_07_27_2021.txt', 'r') as file:
 #         fir = np.array(file.read().split('\n')[:-1], dtype='float64')
 #     sound, _ = t.get_raw()
 #     sound_re = lsfm.inv_fir(sound, fir)
 #     sound_re = t.cut(sound_re)
 #     scipy.io.savemat(f'{filename}_invfir4cwt.mat', {'stim':sound_re})
-#     
-#     cwt = scipy.io.loadmat('0730_fir_cwt.mat')
 # =============================================================================
-
-
-
-
-
+    
+# =============================================================================
+#     """plot stimulus power spectrum"""
+#     cwt = scipy.io.loadmat('20220216_afterCWT')
+# 
+#     f = cwt['f']
+#     f = f[:,0]
+#     wt = cwt['wt'].T[:,0]
+#     wt_a = []
+#     
+#     for w in wt:
+#         wt_a.append(w)
+#     
+#     wt_a = np.array(wt_a)
+#     ylabel = [round((i),2) for i in f]
+#     
+#     
+#     x = np.arange(500)
+#     plt.pcolormesh(x, f, wt_a[101], cmap='hot_r', vmax=0.05, vmin=0)
+#     plt.yscale('log')
+#     plt.xlim(0,375)
+#     plt.xlabel('time (sec)', fontsize=18)
+#     plt.ylabel('frequency (kHz)', fontsize=18)
+#     plt.xticks([0,125,250,375], labels=[0,0.5,1,1.5], rotation=45, fontsize=16)
+#     plt.ylim(1000,100000)
+#     plt.yticks([3000,6000,12000,24000,48000], [3,6,12,24,48], fontsize=16)
+#     plt.title('Center Frequency: 12 kHz, bandwidth: 3 octaves, modulation rate: 8 Hz')
+#     plt.savefig('sample_lsfm_spectrum.pdf', dpi=500, format='pdf', bbox_inches='tight')
+# =============================================================================
+    
+    
